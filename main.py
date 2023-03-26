388cc8aecaa60dd2cd7b8b2d12ca0c6d6caace71
dddd-onigiri <dddd.sailing470@gmail.com>
2023-03-26 21:11:45 +0900

    修正

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import japanize_matplotlib
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
